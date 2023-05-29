e521a39c67b9bd8637b3aeb4d173ff885d865d73
wam <ami6577@colorverse.co>
2023-05-29 09:20:54 +0900

    #1.5 Styles JSX

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,23 +7,30 @@ export default function NavBar() {
 
   return (
     <nav className={styles.nav}>
-      <Link
-        href="/"
-        className={`${styles.link} ${
-          router.pathname === "/" ? styles.active : ""
-        }`}
-      >
-        Home
+      <Link href="/">
+        <span className={router.pathname === "/" ? "active" : ""}>Home</span>
       </Link>
-      <Link
-        href="/about"
-        className={[
-          styles.link,
-          router.pathname === "/about" ? styles.active : ""
-        ].join(" ")}
-      >
-        About
+      <Link href="/about">
+        <span className={router.pathname === "/about" ? "active" : ""}>
+          About
+        </span>
       </Link>
+
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 }
+
+/* 
+  - styled jsx는 NextJS 고유의 방법
+  - <style>태그 (HTML 태그) 사용
+  - 컴포넌트마다 독릭접적으로 사용 가능
+  - 컴포넌트 내부 한정 범위이다.
+*/
