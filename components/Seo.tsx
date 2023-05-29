8170e61739181a8d2b50a7ea31316ce8eb8d64ca
wam <ami6577@colorverse.co>
2023-05-30 05:31:12 +0900

    타이틀 태그 경고 해결
     : Warning: A title element received an array with more than 1 element as children.

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -2,9 +2,11 @@ import Head from "next/head";
 import RoutePath from "./RoutePath";
 
 export default function Seo() {
+  const fullTitle = `${RoutePath()} | Next Movies`;
+
   return (
     <Head>
-      <title>{RoutePath()} | Next Movies</title>
+      <title>{fullTitle}</title>
     </Head>
   );
 }
